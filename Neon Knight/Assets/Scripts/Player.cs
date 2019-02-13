f8c7bef890e9420088086cf90cac907f905837b1
James Bryan <jbryan55555@gmail.com>
2019-02-13 14:08:12 +0000

    Project file Upload

---
+++ Neon Knight/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+
+    protected Joystick joyStick1;
+
+    public float PlayerSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joyStick1 = FindObjectOfType<Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = new Vector3(joyStick1.Horizontal * PlayerSpeed, rigidbody.velocity.y, joyStick1.Vertical * PlayerSpeed);
+    }
+
+  
+}
