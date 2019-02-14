1d144de3f434b368ed9985aa28e4dddc20db206a
James Bryan <jbryan55555@gmail.com>
2019-02-14 16:51:21 +0000

    Small changes

--- Neon Knight/Assets/Scripts/Player.cs
+++ Neon Knight/Assets/Scripts/Player.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    protected Joystick joyStick1;
+    public FixedJoystick joyStick1;
+
+    public FixedJoystick joyStick2;
+
+ 
 
     public float PlayerSpeed;
 
@@ -14,7 +18,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        joyStick1 = FindObjectOfType<Joystick>();
+
     }
 
     // Update is called once per frame
@@ -23,7 +27,11 @@ public class Player : MonoBehaviour
         var rigidbody = GetComponent<Rigidbody>();
 
         rigidbody.velocity = new Vector3(joyStick1.Horizontal * PlayerSpeed, rigidbody.velocity.y, joyStick1.Vertical * PlayerSpeed);
+
+
+
+
+
     }
 
-  
 }
