4e956d4e4fd121e087c86a433b0a298f6e1b44e0
James Bryan <jbryan55555@gmail.com>
2019-02-20 20:29:15 +0000

    testing movement

--- Neon Knight/Assets/Scripts/Player.cs
+++ Neon Knight/Assets/Scripts/Player.cs
@@ -1,37 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 
 public class Player : MonoBehaviour
 {
 
-    public FixedJoystick joyStick1;
 
-    public FixedJoystick joyStick2;
-
- 
-
-    public float PlayerSpeed;
-
+    public float moveForce;
+    Rigidbody myBody;
 
     // Start is called before the first frame update
     void Start()
     {
 
+        myBody = this.GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        var rigidbody = GetComponent<Rigidbody>();
 
-        rigidbody.velocity = new Vector3(joyStick1.Horizontal * PlayerSpeed, rigidbody.velocity.y, joyStick1.Vertical * PlayerSpeed);
-
+        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+        myBody.AddForce(moveVec);
 
 
 
 
     }
+
 
 }
