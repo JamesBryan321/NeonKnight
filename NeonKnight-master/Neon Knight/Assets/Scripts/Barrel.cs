14ede755a95a696c2bed6ca8c6da2312621000a4
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2019-05-14 18:50:44 +0100

    v

---
+++ NeonKnight-master/Neon Knight/Assets/Scripts/Barrel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    private bool exploded = false;
+    public GameObject explosionEffect;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (!exploded)
+        {
+            Explode();
+            exploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Debug.Log("Boom!");
+        Destroy(gameObject);
+    }
+}
