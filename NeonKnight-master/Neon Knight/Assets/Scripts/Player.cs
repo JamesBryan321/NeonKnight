ade1cda1cae6cd803b98fce811a11c1a09eea5b0
James Bryan <jbryan55555@gmail.com>
2019-05-17 01:47:10 +0100

    Final push 2.0 last few things edited

--- NeonKnight-master/Neon Knight/Assets/Scripts/Player.cs
+++ NeonKnight-master/Neon Knight/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 
 
 public class Player : MonoBehaviour
@@ -148,7 +148,7 @@ public class Player : MonoBehaviour
 
         if(PlayerHealth <= 0)
         {
-
+            SceneManager.LoadScene("Menu1");
         }
     }
    
