68ab817f4efb7cdfbb2b499e77e43063662af319
James Bryan <jamesbryangamess@gmail.com>
2021-04-18 21:11:55 +0100

    new tutorial added and other small changes

---
+++ NeonKnight-master/Neon Knight/Assets/Scripts/New/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public Animator DoorAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DoorAnim.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Open door");
+            Debug.Log("Open door");
+            DoorAnim.SetTrigger("Open");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            DoorAnim.SetTrigger("closed");
+        }
+    }
+}
