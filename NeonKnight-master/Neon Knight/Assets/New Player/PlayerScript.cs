067258a57d5c7bb5a9dc05e7e815aee672dcadde
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2021-05-05 00:09:33 +0100

    Player rotates with right analogue stick input, can shoot bullets with r2 (using ld bullet prefab for the moment)

---
+++ NeonKnight-master/Neon Knight/Assets/New Player/PlayerScript.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Gamepad _gamepad;
+
+    private Vector2 _leftStickInput, _rightStickInput;
+    private Vector3 _moveDirection, _lookDirection;
+
+    private Rigidbody _rB;
+    [SerializeField] private GameObject rotatable;
+    public float speed;
+
+    void Awake()
+    {
+        _rB = gameObject.GetComponent<Rigidbody>();
+        
+        _gamepad = Gamepad.current;
+        if (_gamepad != null)
+        {
+            Debug.Log("Gamepad Detected.");
+        }
+    }
+
+    void Update()
+    {
+        
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _leftStickInput = _gamepad.leftStick.ReadValue();
+        _rightStickInput = _gamepad.rightStick.ReadValue();
+        
+        _rB.velocity = new Vector3(_leftStickInput.x * speed, _rB.velocity.y, _leftStickInput.y * speed);
+        
+        _lookDirection =  Vector3.right * _rightStickInput.x + Vector3.forward * _rightStickInput.y;
+
+        if (_lookDirection.sqrMagnitude > 0.0f)
+        {
+            rotatable.transform.rotation = Quaternion.LookRotation(_lookDirection, Vector3.up);
+        }
+
+        /*if (_gamepad.rightTrigger.wasPressedThisFrame)
+        {
+            Debug.Log("Shoot!");
+        }*/
+    }
+}
---
+++ NeonKnight-master/Neon Knight/Assets/New Player/ShootScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShootScript : MonoBehaviour
+{
+    private Gamepad _gamepad;
+
+    [SerializeField] private GameObject bulletPrefab, bulletSpawnPositionObject;
+    
+    void Start()
+    {
+        _gamepad = Gamepad.current;
+        if (_gamepad != null)
+        {
+            Debug.Log("Gamepad Detected.");
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_gamepad.rightTrigger.wasPressedThisFrame)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bulletPrefab, bulletSpawnPositionObject.transform.position, bulletSpawnPositionObject.transform.rotation);
+    }
+}
